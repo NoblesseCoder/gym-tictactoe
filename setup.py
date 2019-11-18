8fbc44b284425e2e1d982078c4825a6e92bff1fe
Ashik Poovanna <ashobot@gmail.com>
2019-11-18 17:02:31 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='gym_tictactoe',
       version='0.0.1',
-      url="https://github.com/yagamiash/gym-tictactoe",
+      url="https://github.com/NoblesseCoder/gym-tictactoe",
       author="Ashik Poovanna",
       description='Tic-Tac-Toe environment for OpenAI Gym',
       license='MIT License',
